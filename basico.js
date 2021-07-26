d6acdf5e0d2c5fc5dcc6d5220e4a53fd92d3e24e
Laiane Silva <laianes1914@gmail.com>
2021-07-26 20:46:50 -0300

    NTRODUÇÂO
    JavaScript básico.

---
+++ basico.js
@@ -0,0 +1,16 @@
+// Tipos primários.
+// number (número), string (caractere), bool (boleano) e null (nulo)
+
+const nome = 'Laiane Silva'
+const idade = 25
+const brasileira = true
+
+const data = {
+  name: nome,
+  age: idade,
+  brazilian: brasileira
+}
+
+console.log('---') // mostra a saida na tela
+console.log('Nome: ' + nome) // concatenacao
+
