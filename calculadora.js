1d7d58b5d188335c4ede932efe8d71bac5e9ddee
Laiane Silva <laianes1914@gmail.com>
2021-07-26 22:23:10 -0300

    Exemplo operaçoes matematicas.
    - Exwmplo de soma, subtraçao, multiplicaçao, divisao, potencia e raiz

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+// TODO: Declara 02 variaves para valor A e valor B. (valorA, valorB)
+
+// TODO: Declarar 04 variaveis para ações de calculadora (soma, subtracao, multiplicacao, divisao)
+
+// TODO: Mostrar os resultados:
+// EX.:  A soma de 2 + 2 = 4.
+
+const valorA = 4
+const valorB = 4
+
+const soma = (valorA  + valorB)
+const subtracao = (valorA - valorB)
+const multiplicacao = (valorA * valorB)
+const divisao = (valorA / valorB)
+const potencia = Math.pow(valorA, valorB)
+const raiz = Math.sqrt(valorA) 
+
+console.log('A soma de ' + valorA + ' + ' + valorB + ' é ' + soma + '!')
+console.log('A subtracao de ' + valorA + ' - ' + valorB + ' é ' + subtracao + '!' )
+console.log('A multiplicacao de ' + valorA + ' * ' + valorB + ' é ' + multiplicacao + '!')
+console.log( 'A divisao de ' + valorA + ' / ' + valorB + ' é ' + divisao + '!')
+console.log('A potencia de ' + valorA + ' por ' + valorB + ' é ' + potencia + '!')
+console.log('A raiz de ' + valorA + ' é igual a ' + raiz + '!')
